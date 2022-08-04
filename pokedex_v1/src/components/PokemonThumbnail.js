4d328f46ad7f34646257a8f5ec848a56626b6c97
JojuGeorge <jojugeorge001@gmail.com>
2022-08-04 10:07:20 +0530

    check safe sprite url and sample click functionality for thumbnail

--- pokedex_v1/src/components/PokemonThumbnail.js
+++ pokedex_v1/src/components/PokemonThumbnail.js
@@ -1,10 +1,22 @@
 import React from "react";
 
 export const PokemonThumbnail = ({ id, name, sprite, type }) => {
+  // checks if the sprite url is safe
+  const isSafe = (spriteUrl) => {
+    if (spriteUrl.startsWith("http://") || spriteUrl.startsWith("https://")) {
+      return spriteUrl;
+    } else {
+      return "https://" + spriteUrl;
+    }
+  };
+
   return (
-    <div className={`pokemon-thumbnail ${type}`}>
+    <div
+      className={`pokemon-thumbnail ${type}`}
+      onClick={() => console.log("clicked ", name)}
+    >
       <div>#{id}</div>
-      <img src={sprite} className="sprite" />
+      <img src={isSafe(sprite)} className="sprite" />
       <div>
         <h3>{name}</h3>
         <p>{type}</p>
